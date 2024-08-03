3410ac8f48a5761eed905752da8062d44640dd7a
Manchick0 <endercreep109@gmail.com>
2024-08-03 13:49:32 +0200

    Add initial project setup
    Created the initial project structure.

---
+++ src/main/java/com/manchick/bagged/Bagged.java
@@ -0,0 +1,10 @@
+package com.manchick.bagged;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Bagged implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
---
+++ src/main/java/com/manchick/bagged/client/BaggedClient.java
@@ -0,0 +1,10 @@
+package com.manchick.bagged.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BaggedClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
